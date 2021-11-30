3185e62ba88efd24531486ec85a08609dd43a3fe
FogFoge <62325603+FogFoge@users.noreply.github.com>
2021-11-30 23:22:00 +0800

    Add files via upload

--- js/fr_FR.9e0db5fa.js
+++ js/fr_FR.9e0db5fa.js
@@ -1,1 +1,55 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["fr_FR"],{"46cf":function(e,n,o){n=e.exports=o("2350")(!1),n.push([e.i,".content[data-v-3a3d06c4]{position:relative}.content .button-wrapper[data-v-3a3d06c4]{position:absolute;top:30px;right:10px}",""])},d611:function(e,n,o){var a=o("46cf");a.__esModule&&(a=a.default),"string"===typeof a&&(a=[[e.i,a,""]]),a.locals&&(e.exports=a.locals);var r=o("499e").default;r("106083fa",a,!0,{sourceMap:!1,shadowMode:!1})},dbbc:function(e,n){e.exports='<h1>996.ICU</h1>\n<h2>Qu&#39;est-ce que « 996 » ?</h2>\n<p>Un emploi « 996 » fait référence à un régime  de travail non officiel (de 9h à 21h, 6 jours par semaine) qui gagne en popularité en Chine.\nTravailler pour une entreprise qui encourage le modèle de travail « 996 » signifie en général travailler au moins 60 heures par semaine.</p>\n<h2>Loi du travail de la République populaire de Chine</h2>\n<p>L&#39;article 41 de la <a href="http://www.china.org.cn/living_in_china/abc/2009-07/15/content_18140508.htm">Loi du travail de la République populaire de Chine</a> précise :</p>\n<blockquote>\n<p>L&#39;employeur a le droit de prolonger les horaires de travail pour des besoins de production après consultation de son syndicat et de ses employés. En général, le prolongement des horaires de travail ne doit pas dépasser une heure par jour, ou trois heures par jour si cette prolongation est demandée pour des raisons particulières et à condition que la santé physique des employés soit garantie. Le prolongement du temps de travail ne doit toutefois pas dépasser 36 heures par mois.</p>\n</blockquote>\n<h2>Gain de popularité</h2>\n<p>En novembre 2018, <a href="jumei.com">Jumei</a> a commencé à adopter le régime de travail « 996 » et à le considérer comme les horaires de travail normales. La société refuse toute rémunération pour les heures supplémentaires et ne donne aucun jour de congé payé. Même un jour de congé sans solde doit compter 11 heures au lieu de 8 heures. Le samedi, les employés doivent envoyer un e-mail au directeur du département pour approuver le &quot;congé&quot;. En janvier 2019, les employés d&#39;un nouveau groupe de Jumei appelé Wangwang ont été forcés à abandonner tous les congés, la durée du service. Ils étaient par ailleurs obligés de transferer le contrat de travail à Jumei, sans quoi ils étaient renvoyés.</p>\n<p>Début 2019, une entreprise d&#39;e-commerce chinoise appelée <em>Youzan</em> a déclaré son adoption au régime de travail « 996 » lors de la fête du Nouvel An Chinois de la société. Le PDG de Youzan a annoncé que ce sera certainement une bonne décision si une rétrospective est faite quelques années plus tard.</p>\n<p>Mi-mars 2019, il a été signalé que <a href="http://jd.com">Jingdong</a> avait commencé à adopter le mode de fonctionnement « 996 » ou « 995 » dans certaines unités commerciales. Le département des relations publiquess chez JD a publié sur son compte maimai (<code>脉脉</code>, une plate-forme de réseau social d&#39;entreprises chinoises): « (Notre culture consiste à) nous consacrer de tout notre cœur (à atteindre les objectifs de l&#39;entreprise). »</p>\n<h2>Compensation et récompense</h2>\n<p>Selon la loi du travail en Chine, les employés qui suivent le schéma de travail « 996 » méritent de toucher 2,275 fois leur salaire de base (donc plus du double). Malheureusement, ceux qui sont au régime « 996 » sont rarement payés autant.</p>\n<h2>D&#39;où vient le nom du repo <code>996.ICU</code> (<code>996.USI</code> en français) ?</h2>\n<p>A cause du mode de travail « 996 », beaucoup de travailleurs risquent de finir dans l&#39;<strong>U</strong>nité de <strong>S</strong>oins <strong>I</strong>ntensifs (<strong>I</strong>ntensive <strong>C</strong>are <strong>U</strong>nit en anglais).</p>\n<p>La vie des développeurs compte.</p>\n'},ef76:function(e,n,o){"use strict";o.r(n);var a=function(){var e=this,n=e.$createElement,o=e._self._c||n;return o("div",{staticClass:"content markdown-body override"},[o("section",{domProps:{innerHTML:e._s(e.content)}})])},r=[],t=o("dbbc"),s=o.n(t),i={data:function(){return{content:s.a}}},u=i,l=(o("f474"),o("2877")),d=Object(l["a"])(u,a,r,!1,null,"3a3d06c4",null);n["default"]=d.exports},f474:function(e,n,o){"use strict";o("d611")}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["fr_FR"], {
+        "46cf": function (e, n, o) {
+            n = e.exports = o("2350")(!1), n.push([e.i, ".content[data-v-3a3d06c4]{position:relative}.content .button-wrapper[data-v-3a3d06c4]{position:absolute;top:30px;right:10px}", ""])
+        },
+        d611: function (e, n, o) {
+            var a = o("46cf");
+            a.__esModule && (a = a.default), "string" === typeof a && (a = [
+                [e.i, a, ""]
+            ]), a.locals && (e.exports = a.locals);
+            var r = o("499e").default;
+            r("106083fa", a, !0, {
+                sourceMap: !1,
+                shadowMode: !1
+            })
+        },
+        dbbc: function (e, n) {
+            e.exports = '<h1>996.ICU</h1>\n<h2>Qu&#39;est-ce que « 996 » ?</h2>\n<p>Un emploi « 996 » fait référence à un régime  de travail non officiel (de 9h à 21h, 6 jours par semaine) qui gagne en popularité en Chine.\nTravailler pour une entreprise qui encourage le modèle de travail « 996 » signifie en général travailler au moins 60 heures par semaine.</p>\n<h2>Loi du travail de la République populaire de Chine</h2>\n<p>L&#39;article 41 de la <a href="http://www.china.org.cn/living_in_china/abc/2009-07/15/content_18140508.htm">Loi du travail de la République populaire de Chine</a> précise :</p>\n<blockquote>\n<p>L&#39;employeur a le droit de prolonger les horaires de travail pour des besoins de production après consultation de son syndicat et de ses employés. En général, le prolongement des horaires de travail ne doit pas dépasser une heure par jour, ou trois heures par jour si cette prolongation est demandée pour des raisons particulières et à condition que la santé physique des employés soit garantie. Le prolongement du temps de travail ne doit toutefois pas dépasser 36 heures par mois.</p>\n</blockquote>\n<h2>Gain de popularité</h2>\n<p>En novembre 2018, <a href="jumei.com">Jumei</a> a commencé à adopter le régime de travail « 996 » et à le considérer comme les horaires de travail normales. La société refuse toute rémunération pour les heures supplémentaires et ne donne aucun jour de congé payé. Même un jour de congé sans solde doit compter 11 heures au lieu de 8 heures. Le samedi, les employés doivent envoyer un e-mail au directeur du département pour approuver le &quot;congé&quot;. En janvier 2019, les employés d&#39;un nouveau groupe de Jumei appelé Wangwang ont été forcés à abandonner tous les congés, la durée du service. Ils étaient par ailleurs obligés de transferer le contrat de travail à Jumei, sans quoi ils étaient renvoyés.</p>\n<p>Début 2019, une entreprise d&#39;e-commerce chinoise appelée <em>Youzan</em> a déclaré son adoption au régime de travail « 996 » lors de la fête du Nouvel An Chinois de la société. Le PDG de Youzan a annoncé que ce sera certainement une bonne décision si une rétrospective est faite quelques années plus tard.</p>\n<p>Mi-mars 2019, il a été signalé que <a href="http://jd.com">Jingdong</a> avait commencé à adopter le mode de fonctionnement « 996 » ou « 995 » dans certaines unités commerciales. Le département des relations publiquess chez JD a publié sur son compte maimai (<code>脉脉</code>, une plate-forme de réseau social d&#39;entreprises chinoises): « (Notre culture consiste à) nous consacrer de tout notre cœur (à atteindre les objectifs de l&#39;entreprise). »</p>\n<h2>Compensation et récompense</h2>\n<p>Selon la loi du travail en Chine, les employés qui suivent le schéma de travail « 996 » méritent de toucher 2,275 fois leur salaire de base (donc plus du double). Malheureusement, ceux qui sont au régime « 996 » sont rarement payés autant.</p>\n<h2>D&#39;où vient le nom du repo <code>996.ICU</code> (<code>996.USI</code> en français) ?</h2>\n<p>A cause du mode de travail « 996 », beaucoup de travailleurs risquent de finir dans l&#39;<strong>U</strong>nité de <strong>S</strong>oins <strong>I</strong>ntensifs (<strong>I</strong>ntensive <strong>C</strong>are <strong>U</strong>nit en anglais).</p>\n<p>La vie des développeurs compte.</p>\n<p>Cette page est une image miroir de la 996.ICU et est destinée à soutenir mentalement Github @vivien8261</p>\n'
+        },
+        ef76: function (e, n, o) {
+            "use strict";
+            o.r(n);
+            var a = function () {
+                    var e = this,
+                        n = e.$createElement,
+                        o = e._self._c || n;
+                    return o("div", {
+                        staticClass: "content markdown-body override"
+                    }, [o("section", {
+                        domProps: {
+                            innerHTML: e._s(e.content)
+                        }
+                    })])
+                },
+                r = [],
+                t = o("dbbc"),
+                s = o.n(t),
+                i = {
+                    data: function () {
+                        return {
+                            content: s.a
+                        }
+                    }
+                },
+                u = i,
+                l = (o("f474"), o("2877")),
+                d = Object(l["a"])(u, a, r, !1, null, "3a3d06c4", null);
+            n["default"] = d.exports
+        },
+        f474: function (e, n, o) {
+            "use strict";
+            o("d611")
+        }
+    }
+]);
