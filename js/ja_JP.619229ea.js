3919d3257450a281617b04e8b8201fdf7a0624dd
FogFoge <62325603+FogFoge@users.noreply.github.com>
2021-11-30 23:22:38 +0800

    Add files via upload

--- js/ja_JP.619229ea.js
+++ js/ja_JP.619229ea.js
@@ -1,1 +1,55 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["ja_JP"],{"12d2":function(n,o,t){var p=t("57a3");p.__esModule&&(p=p.default),"string"===typeof p&&(p=[[n.i,p,""]]),p.locals&&(n.exports=p.locals);var r=t("499e").default;r("376ceb38",p,!0,{sourceMap:!1,shadowMode:!1})},1824:function(n,o,t){"use strict";t.r(o);var p=function(){var n=this,o=n.$createElement,t=n._self._c||o;return t("div",{staticClass:"content markdown-body override"},[t("section",{domProps:{innerHTML:n._s(n.content)}})])},r=[],s=t("c389"),e=t.n(s),c={data:function(){return{content:e.a}}},a=c,u=(t("72bd"),t("2877")),b=Object(u["a"])(a,p,r,!1,null,"276b0b1a",null);o["default"]=b.exports},"57a3":function(n,o,t){o=n.exports=t("2350")(!1),o.push([n.i,".content[data-v-276b0b1a]{position:relative}.content .button-wrapper[data-v-276b0b1a]{position:absolute;top:30px;right:10px}",""])},"72bd":function(n,o,t){"use strict";t("12d2")},c389:function(n,o){n.exports='<h1>996.ICU</h1>\n<p>「996」とは、朝9時から夜9時まで働き、週6日間勤務体制であります。\n それより、週に最低6x12=72時間の働きでなければなりません。</p>\n<p><strong>中国標準的な労働時間：</strong>\n労働時間は1日8時間で, 平均週40時間と制限されます。残業上限は1日3時間、及び月36時間と制限されます。\nまた、法律により、残業の給料は平日の給料の最低でも150%にしなければなりません。\n1週間の最大労働時間を参考にし、労働時間が週48時間の場合、平均月給日数は21.75日です。</p>\n<p><strong>「中華人民共和国労働法」</strong>（以下 <strong>「労働法」</strong> と称する）<strong>第四章第三十六条</strong>：</p>\n<blockquote>\n<p>国は労働者の毎日の労働時間が8時間を超えないと決め、毎週の平均労働時間が44時間を超えない労働時間制度を実行する。</p>\n</blockquote>\n<p><strong>「労働法」第四章第三十九条</strong>：</p>\n<blockquote>\n<p>企業は生産の特殊性により本法第36条、第38条の規定を実行することができない場合、労働行政部門の承認を得た上で、その他の労働時間と休暇方法を実行することができる。</p>\n</blockquote>\n<p><strong>「労働法」第四章第四十一条</strong>：</p>\n<blockquote>\n<p>雇用側の組織は生産経営の必要により、労働組合及び労働者との協議を経た後に労働時間を延長することができるが、原則として毎日が一時間を超えてはならない;\n特殊な原因により、労働時間の延長が必要な時は、労働者の身体健康を保障することの条件の上で延長する労働時間は毎日が三時間を超えてはならない。ただし、毎月が36時間を超えてはならない。</p>\n</blockquote>\n<p><strong>「労働法」第四章第四十三条</strong>：</p>\n<blockquote>\n<p>雇用側の組織は本法の規定に違反して労働者の労働時間を延長してはならない。</p>\n</blockquote>\n<p><strong>「労働法」第四章第四十四条</strong>：</p>\n<blockquote>\n<p>下記の状況のいずれかにあった場合、雇用側は下記の規準に従って労働者の正常な労働時間の賃金より高い賃金を払うべきである<br>（A）労働者に労働時間の延長を手配した場合、賃金の100分の150より低くない賃金を払う<br>（B）休息日に労働者に業務を手配した場合、または代休を処置できない場合、賃金の100分の200より低くない賃金を払う<br>（C）法定休暇日に労働者に業務を手配した場合、賃金の100分の300より低くない賃金を払う</p>\n</blockquote>\n<p><strong>「労働法」第十二章第九十条</strong>：</p>\n<blockquote>\n<p>雇用側が本法の規定に違反し、労働者の労働時間を延長した時は、労働行政部門が警告を与えて改正を命じ、その上で罰金を科すことができる。</p>\n</blockquote>\n<p><strong>「労働法」第十二章第九十一条</strong>：</p>\n<blockquote>\n<p>雇用側に下記の労働者の合法的権益を侵害する状況のいずれかがあれば、労働行政部門が労働者の賃金、経済的補償の支払を命じ、併せて賠償金の支払を命じることができる。<br>……<br>（二） <strong>労働者に労働時間延長の賃金報酬の支払を拒み行わない時</strong><br>……</p>\n</blockquote>\n<p><strong>関連記事</strong></p>\n<p>2016年9月の初から、中国の就活・情報大手企業である「58同城」がすべての従業員に対して「996」勤務体制を実施し、休日出勤の賃金を払わない報告は相次いで出てきました。\n同社により、ピーク時の取引量に対処するために、毎年9月と10月に動員するという定期的な行動があります。この「996動員」は強引ではないとも述べました。</p>\n<p>2019年1月に杭州のEC会社「有賛」は年次総会で、来年度に996勤務体制を導入すると発表しました。\n最高経営責任者（CEO）白鴉より、「数年後から振り返れば、これ（996）は間違えなくいい制度であったと言えるでしょう！」と述べました。</p>\n<p>2019年3月中旬、京東商城(JD)は部門別に996または995勤務体制を実施し始めました。\nJDの広報部門が脉脉（maimai、中国のビジネスSNS）で「全心を込め、仕事に尽す。」と述べません。</p>\n<p>労働法によりますと、996勤務体制では現時点の給料の2.275倍に増やさないと損します。</p>\n<p><strong>996.ICUとは？仕事は996で、病気でICUへ。</strong></p>\n<p>参考：<a href="http://www.npc.gov.cn/npc/xinwen/2019-01/07/content_2070261.htm">中華人民共和国労働法（2018年改訂）</a></p>\n<p><strong>労働者の命は何よりも大切です。</strong></p>\n'}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["ja_JP"], {
+        "12d2": function (n, o, t) {
+            var p = t("57a3");
+            p.__esModule && (p = p.default), "string" === typeof p && (p = [
+                [n.i, p, ""]
+            ]), p.locals && (n.exports = p.locals);
+            var r = t("499e").default;
+            r("376ceb38", p, !0, {
+                sourceMap: !1,
+                shadowMode: !1
+            })
+        },
+        1824: function (n, o, t) {
+            "use strict";
+            t.r(o);
+            var p = function () {
+                    var n = this,
+                        o = n.$createElement,
+                        t = n._self._c || o;
+                    return t("div", {
+                        staticClass: "content markdown-body override"
+                    }, [t("section", {
+                        domProps: {
+                            innerHTML: n._s(n.content)
+                        }
+                    })])
+                },
+                r = [],
+                s = t("c389"),
+                e = t.n(s),
+                c = {
+                    data: function () {
+                        return {
+                            content: e.a
+                        }
+                    }
+                },
+                a = c,
+                u = (t("72bd"), t("2877")),
+                b = Object(u["a"])(a, p, r, !1, null, "276b0b1a", null);
+            o["default"] = b.exports
+        },
+        "57a3": function (n, o, t) {
+            o = n.exports = t("2350")(!1), o.push([n.i, ".content[data-v-276b0b1a]{position:relative}.content .button-wrapper[data-v-276b0b1a]{position:absolute;top:30px;right:10px}", ""])
+        },
+        "72bd": function (n, o, t) {
+            "use strict";
+            t("12d2")
+        },
+        c389: function (n, o) {
+            n.exports = '<h1>996.ICU</h1>\n<p>「996」とは、朝9時から夜9時まで働き、週6日間勤務体制であります。\n それより、週に最低6x12=72時間の働きでなければなりません。</p>\n<p><strong>中国標準的な労働時間：</strong>\n労働時間は1日8時間で, 平均週40時間と制限されます。残業上限は1日3時間、及び月36時間と制限されます。\nまた、法律により、残業の給料は平日の給料の最低でも150%にしなければなりません。\n1週間の最大労働時間を参考にし、労働時間が週48時間の場合、平均月給日数は21.75日です。</p>\n<p><strong>「中華人民共和国労働法」</strong>（以下 <strong>「労働法」</strong> と称する）<strong>第四章第三十六条</strong>：</p>\n<blockquote>\n<p>国は労働者の毎日の労働時間が8時間を超えないと決め、毎週の平均労働時間が44時間を超えない労働時間制度を実行する。</p>\n</blockquote>\n<p><strong>「労働法」第四章第三十九条</strong>：</p>\n<blockquote>\n<p>企業は生産の特殊性により本法第36条、第38条の規定を実行することができない場合、労働行政部門の承認を得た上で、その他の労働時間と休暇方法を実行することができる。</p>\n</blockquote>\n<p><strong>「労働法」第四章第四十一条</strong>：</p>\n<blockquote>\n<p>雇用側の組織は生産経営の必要により、労働組合及び労働者との協議を経た後に労働時間を延長することができるが、原則として毎日が一時間を超えてはならない;\n特殊な原因により、労働時間の延長が必要な時は、労働者の身体健康を保障することの条件の上で延長する労働時間は毎日が三時間を超えてはならない。ただし、毎月が36時間を超えてはならない。</p>\n</blockquote>\n<p><strong>「労働法」第四章第四十三条</strong>：</p>\n<blockquote>\n<p>雇用側の組織は本法の規定に違反して労働者の労働時間を延長してはならない。</p>\n</blockquote>\n<p><strong>「労働法」第四章第四十四条</strong>：</p>\n<blockquote>\n<p>下記の状況のいずれかにあった場合、雇用側は下記の規準に従って労働者の正常な労働時間の賃金より高い賃金を払うべきである<br>（A）労働者に労働時間の延長を手配した場合、賃金の100分の150より低くない賃金を払う<br>（B）休息日に労働者に業務を手配した場合、または代休を処置できない場合、賃金の100分の200より低くない賃金を払う<br>（C）法定休暇日に労働者に業務を手配した場合、賃金の100分の300より低くない賃金を払う</p>\n</blockquote>\n<p><strong>「労働法」第十二章第九十条</strong>：</p>\n<blockquote>\n<p>雇用側が本法の規定に違反し、労働者の労働時間を延長した時は、労働行政部門が警告を与えて改正を命じ、その上で罰金を科すことができる。</p>\n</blockquote>\n<p><strong>「労働法」第十二章第九十一条</strong>：</p>\n<blockquote>\n<p>雇用側に下記の労働者の合法的権益を侵害する状況のいずれかがあれば、労働行政部門が労働者の賃金、経済的補償の支払を命じ、併せて賠償金の支払を命じることができる。<br>……<br>（二） <strong>労働者に労働時間延長の賃金報酬の支払を拒み行わない時</strong><br>……</p>\n</blockquote>\n<p><strong>関連記事</strong></p>\n<p>2016年9月の初から、中国の就活・情報大手企業である「58同城」がすべての従業員に対して「996」勤務体制を実施し、休日出勤の賃金を払わない報告は相次いで出てきました。\n同社により、ピーク時の取引量に対処するために、毎年9月と10月に動員するという定期的な行動があります。この「996動員」は強引ではないとも述べました。</p>\n<p>2019年1月に杭州のEC会社「有賛」は年次総会で、来年度に996勤務体制を導入すると発表しました。\n最高経営責任者（CEO）白鴉より、「数年後から振り返れば、これ（996）は間違えなくいい制度であったと言えるでしょう！」と述べました。</p>\n<p>2019年3月中旬、京東商城(JD)は部門別に996または995勤務体制を実施し始めました。\nJDの広報部門が脉脉（maimai、中国のビジネスSNS）で「全心を込め、仕事に尽す。」と述べません。</p>\n<p>労働法によりますと、996勤務体制では現時点の給料の2.275倍に増やさないと損します。</p>\n<p><strong>996.ICUとは？仕事は996で、病気でICUへ。</strong></p>\n<p>参考：<a href="http://www.npc.gov.cn/npc/xinwen/2019-01/07/content_2070261.htm">中華人民共和国労働法（2018年改訂）</a></p>\n<p><strong>労働者の命は何よりも大切です。</strong></p>\n<p>このページは、Gitub @vivien8261を精神的にサポートする 996.icu のミラーです</p>\n'
+        }
+    }
+]);
